c703dbf17b8d988bc7b6e54934a616aefa06e574
Connor C <164671442+ConnorC3@users.noreply.github.com>
2025-03-07 20:10:35 -0500

    Add operate function and check for divison by 0

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(a, b, op){
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            if (b === 0){
+                return "Error: Division by 0";
+            } else {
+                return divide(a, b);
+            }
+        default:
+            alert("Invalid input");
+            return;
+    }
+}
